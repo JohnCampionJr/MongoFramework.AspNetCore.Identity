6b856667375ef97ce95bdecab41a19dade60abc5
John Campion <john@brightshore.com>
2020-11-03 17:14:32 -0500

    Add Context to Services
    Makes it a little cleaner on the services setup

--- src/MongoFramework.AspNetCore.Identity/MongoIdentityBuilderExtensions.cs
+++ src/MongoFramework.AspNetCore.Identity/MongoIdentityBuilderExtensions.cs
@@ -24,6 +24,7 @@ namespace Microsoft.Extensions.DependencyInjection
         public static IdentityBuilder AddMongoFrameworkStores<TContext>(this IdentityBuilder builder)
             where TContext : IMongoDbContext
         {
+            builder.Services.AddMongoDbContext<TContext>(); // Note, will not override existing context if already added.
             AddStores(builder.Services, builder.UserType, builder.RoleType, typeof(TContext));
             return builder;
         }
