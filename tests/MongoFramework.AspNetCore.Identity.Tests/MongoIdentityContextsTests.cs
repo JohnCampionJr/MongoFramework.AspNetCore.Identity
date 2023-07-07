a657b6dc1da0309fe84739b93affdaff7bb57b50
John Campion Jr <john@brightshore.com>
2023-07-06 20:59:40 -0400

    feat: support net6 and net7
    removed older versions

--- tests/MongoFramework.AspNetCore.Identity.Tests/MongoIdentityContextsTests.cs
+++ tests/MongoFramework.AspNetCore.Identity.Tests/MongoIdentityContextsTests.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
 using MongoFramework.AspNetCore.Identity.Tests.TestClasses;
@@ -7,37 +7,37 @@ using Xunit;
 
 namespace MongoFramework.AspNetCore.Identity.Tests
 {
-	public class MongoIdentityContextsTests : TestBase, IAsyncLifetime
-	{
+    public class MongoIdentityContextsTests : TestBase, IAsyncLifetime
+    {
 
-		public MongoIdentityContextsTests() : base("MongoIdentityContexts") { }
+        public MongoIdentityContextsTests() : base("MongoIdentityContexts") { }
 
-		public async Task InitializeAsync()
-		{
-			var context = new TestContext(GetConnection());
-			var store = new MongoUserStore<MongoIdentityUser>(context);
+        public async Task InitializeAsync()
+        {
+            var context = new TestContext(GetConnection());
+            var store = new MongoUserStore<MongoIdentityUser>(context);
 
-            context.Roles.Add(new MongoIdentityRole {Id = "rid1", Name = "Role 1"});
-            context.Roles.Add(new MongoIdentityRole {Id = "rid2", Name = "Role 2"});
-            context.Roles.Add(new MongoIdentityRole {Id = "rid3", Name = "Role 3"});
+            context.Roles.Add(new MongoIdentityRole { Id = "rid1", Name = "Role 1" });
+            context.Roles.Add(new MongoIdentityRole { Id = "rid2", Name = "Role 2" });
+            context.Roles.Add(new MongoIdentityRole { Id = "rid3", Name = "Role 3" });
 
             await context.SaveChangesAsync();
 
-			var user = TestUser.First;
+            var user = TestUser.First;
             user.Roles.Add("rid1");
             user.Roles.Add("rid2");
-			await store.CreateAsync(user);
+            await store.CreateAsync(user);
 
-		}
+        }
 
-		public Task DisposeAsync() => Task.CompletedTask;
+        public Task DisposeAsync() => Task.CompletedTask;
 
         [Fact]
         public void ContextWithRolesLoadsRoles()
         {
             var context = new MongoIdentityDbContext(GetConnection());
 
-            var store = new MongoRoleStore<MongoIdentityRole,MongoIdentityDbContext>(context);
+            var store = new MongoRoleStore<MongoIdentityRole, MongoIdentityDbContext>(context);
 
             store.Context.ShouldBeOfType<MongoIdentityDbContext>();
             store.Roles.Count().ShouldBe(3);
@@ -48,10 +48,11 @@ namespace MongoFramework.AspNetCore.Identity.Tests
         {
             var context = new MongoIdentityUserContext(GetConnection());
 
-            var store = new MongoUserOnlyStore<MongoIdentityUser,MongoIdentityUserContext>(context);
+            var store = new MongoUserOnlyStore<MongoIdentityUser, MongoIdentityUserContext>(context);
 
             store.Context.ShouldBeOfType<MongoIdentityUserContext>();
             store.Users.Count().ShouldBe(1);
         }
 
-	}}
+    }
+}
